09e5142040761ac6955bf8f0ef41868bb69cb1d5
Agelinena <lucaslindemann88@gmail.com>
2026-03-06 12:54:37 -0300

    arruma erro de init do sql 2

--- 03-apps/appflowy/init.sql
+++ 03-apps/appflowy/init.sql
@@ -1,6 +1,9 @@
 -- Enable uuid-ossp for uuid_generate_v4()
 CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
 
+-- Enable vector for pgvector
+CREATE EXTENSION IF NOT EXISTS "vector";
+
 -- Create the auth schema required by AppFlowy/GoTrue migrations
 CREATE SCHEMA IF NOT EXISTS auth;
 
