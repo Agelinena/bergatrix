d87c5090d83522b57e969206e9a093f3656dcb31
Agelinena <lucaslindemann88@gmail.com>
2026-03-16 15:06:41 -0300

    add original certs yml

--- 04-monitoring/wazuh/indexer-security-init.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-docker exec -it wazuh-indexer bash \
-/usr/share/wazuh-indexer/plugins/opensearch-security/tools/securityadmin.sh \
--cd /usr/share/wazuh-indexer/opensearch-security/ \
--nhnv -cacert /usr/share/wazuh-indexer/certs/root-ca.pem \
--cert /usr/share/wazuh-indexer/certs/admin.pem \
--key /usr/share/wazuh-indexer/certs/admin-key.pem \
--h wazuh.indexer
